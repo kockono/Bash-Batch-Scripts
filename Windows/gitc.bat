753fa16a90ab442dd32ea4876075e003931ea5ee
kockono <kockonoop@gmail.com>
2023-06-24 01:53:00 -0600

    feat: :sparkles: fast commits in windows

--- Windows/gitc.bat
+++ Windows/gitc.bat
@@ -4,29 +4,23 @@ REM Función fast_commit
 :fast_commit
 set tipo_commit=%1
 if "%tipo_commit%"=="f" (
-    echo Descripcion del Feature:
-    set /p commitName=
-    git add . && git commit -m "feat: :sparkles: %commitName%" && git push
+    echo Feature:
+    git add . && git commit -m "feat: :sparkles: %2 %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="fx" (
-    echo Descripcion del Fix:
-    set /p commitName=
-    git add . && git commit -m "fix: :bug: %commitName%" && git push
+    echo Fix:
+    git add . && git commit -m "fix: :bug: %2 %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="d" (
-    echo Descripcion del Documentation:
-    set /p commitName=
-    git add . && git commit -m "docs: :memo: %commitName%" && git push
+    echo Documentation:
+    git add . && git commit -m "docs: :memo: %2 %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="t" (
-    echo Descripcion del Test:
-    set /p commitName=
-    git add . && git commit -m "test: :test_tube: %commitName%" && git push
+    echo Test:
+    git add . && git commit -m "test: :test_tube: %2 %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="d" (
-    echo Descripcion del Remove dead code:
-    set /p commitName=
-    git add . && git commit -m "remove: :coffin: %commitName%" && git push
+    echo Remove dead code:
+    git add . && git commit -m "remove: :coffin: %2 %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="h" (
-    echo Descripcion del HotFix:
-    set /p commitName=
-    git add . && git commit -m "hotfix: :ambulance: %commitName%" && git push
+    echo HotFix:
+    git add . && git commit -m "hotfix: :ambulance: %2 %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="q" (
     echo Saliendo del programa...
     exit 0
