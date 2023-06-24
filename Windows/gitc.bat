834a0685fa6d8b4bdf7c4f1b7021a59e4a6e3576
kockono <kockonoop@gmail.com>
2023-06-24 02:17:35 -0600

    feat: :sparkles: simplificacion de codigo

--- Windows/gitc.bat
+++ Windows/gitc.bat
@@ -2,26 +2,26 @@
 
 REM Función fast_commit
 :fast_commit
-git pull
+git pull && git add .
 set tipo_commit=%1
 if "%tipo_commit%"=="f" (
     echo Feature:
-    git add . && git commit -m "feat: :sparkles: %2 %3 %4 %5 %6 %7 %8 %9" && git push
+    git commit -m "feat: :sparkles: %2 %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="fx" (
     echo Fix:
-    git add . && git commit -m "fix: :bug: %2 %3 %4 %5 %6 %7 %8 %9" && git push
+    git commit -m "fix: :bug: %2 %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="d" (
     echo Documentation:
-    git add . && git commit -m "docs: :memo: %2 %3 %4 %5 %6 %7 %8 %9" && git push
+    git commit -m "docs: :memo: %2 %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="t" (
     echo Test:
-    git add . && git commit -m "test: :test_tube: %2 %3 %4 %5 %6 %7 %8 %9" && git push
+    git commit -m "test: :test_tube: %2 %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="d" (
     echo Remove dead code:
-    git add . && git commit -m "remove: :coffin: %2 %3 %4 %5 %6 %7 %8 %9" && git push
+    git commit -m "remove: :coffin: %2 %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="h" (
     echo HotFix:
-    git add . && git commit -m "hotfix: :ambulance: %2 %3 %4 %5 %6 %7 %8 %9" && git push
+    git commit -m "hotfix: :ambulance: %2 %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="q" (
     echo Saliendo del programa...
     exit 0
