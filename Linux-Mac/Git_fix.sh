7a3adc5fda48a7c2ecac112511f748a3deb80502
kockono <kockonoop@gmail.com>
2023-06-27 13:22:04 -0600

    feat(Git): :sparkles: multiples argumentos test

--- Linux-Mac/Git_fix.sh
+++ Linux-Mac/Git_fix.sh
@@ -6,6 +6,7 @@
 # Función para mostrar el menú principal
 mostrar_menu() {
     echo "Menú de opciones:"
+    echo "a. Crear Commit de todos los cambios y push                : git add . && git commit -m 'CommitName' && git push"
     echo "1. Reiniciar número de commits                             : git reset --hard HEAD~\$numberCommits"
     echo "2. Eliminar archivos de git cache (.vscode, bin, obj)      : git rm --cached . -rf"
     echo "3. Encontrar un git en específico sensitive case           : git log --all --oneline --grep='gitName'"
@@ -21,6 +22,9 @@ procesar_opcion() {
     read -p "Selecciona una opción: " opcion
     local opcion=$1
     case $opcion in
+        a)
+            sub_menu_a
+            ;;
         1)
             echo "¿Número de commits a reiniciar?"
             read numberCommits
@@ -58,40 +62,97 @@ procesar_opcion() {
     esac
 }
 
+# Función para mostrar el submenú de opciones de commits
+sub_menu_a() {
+    echo "Submenú - Opciones de commits:"
+    echo "a. Feature"
+    echo "1. Fix"
+    echo "2. Documentation"
+    echo "3. Test"
+    echo "4. Remove dead code"
+    echo "5. HotFix"
+    echo "q. Regresar al menú principal"
+
+    read -p "Selecciona una opción: " opcion_commit
+    opciones_commits $opcion_commit
+}
+
+opciones_commits() {
+    local tipo_commit=$1
+    echo $(git add .)
+    case $tipo_commit in
+        a)
+            echo "Descripcion del Feature:"
+            read commitName
+            echo $(git commit -m "feat($2): :sparkles: $3 $4 $5 $6 $7 $8 $9" && git push)
+            ;;
+        1)
+            echo "Descripcion del Fix"
+            read commitName  
+            echo $(git commit -m "fix($2): :bug: $3 $4 $5 $6 $7 $8 $9" && git push)
+            ;;
+        2)
+            echo "Descripcion del Documentation"
+            read commitName
+            echo $(git commit -m "docs($2): :memo: $3 $4 $5 $6 $7 $8 $9" && git push)
+            ;;
+        3)
+            echo "Descripcion del Test"
+            read commitName
+            echo $(git commit -m "test($2): :test_tube: $3 $4 $5 $6 $7 $8 $9" && git push)
+            ;;
+        4)
+            echo "Descripcion del Remove dead code"
+            read commitName
+            echo $(git commit -m "remove($2): :coffin: $3 $4 $5 $6 $7 $8 $9" && git push)
+            ;;
+        5)
+            echo "Descripcion del HotFix"
+            read commitName
+            echo $(git commit -m "hotfix($2): :ambulance: $3 $4 $5 $6 $7 $8 $9" && git push)
+            ;;
+        q)
+            echo "Saliendo del programa..."
+            exit 0
+            ;;
+        *)
+            echo "Opción inválida. Por favor, selecciona una opción válida."
+            ;;
+    esac
+}
+
 fast_commit() {
     local tipo_commit=$1
-    local scope=$2
-    local description=$3
-    echo "$tipo_commit, $scope, $description"
-
+    echo $1
+    echo "fast commit"
     case $tipo_commit in
         f)
             echo "Descripcion del Feature:"
-            echo $(git add . && git commit -m "feat($scope): :sparkles: $3 $4 $5 $6 $7 $8 $9" && git push)
+            echo $(git add . && git commit -m "feat($2): :sparkles: $3 $4 $5 $6 $7 $8 $9" && git push)
             ;;
         fx)
             echo "Descripcion del Fix"  
-            echo $(git add . && git commit -m "fix($scope): :bug: $3 $4 $5 $6 $7 $8 $9" && git push)
+            echo $(git add . && git commit -m "fix($2): :bug: $3 $4 $5 $6 $7 $8 $9" && git push)
             ;;
         d)
             echo "Descripcion del Documentation"
-            echo $(git add . && git commit -m "docs($scope): :memo: $3 $4 $5 $6 $7 $8 $9" && git push)
+            echo $(git add . && git commit -m "docs($2): :memo: $3 $4 $5 $6 $7 $8 $9" && git push)
             ;;
         t)
             echo "Descripcion del Test"
-            echo $(git add . && git commit -m "test($scope): :test_tube: $3 $4 $5 $6 $7 $8 $9" && git push)
+            echo $(git add . && git commit -m "test($2): :test_tube: $3 $4 $5 $6 $7 $8 $9" && git push)
             ;;
         r)
             echo "Descripcion del Remove dead code"
-            echo $(git add . && git commit -m "remove($scope): :coffin: $3 $4 $5 $6 $7 $8 $9" && git push)
+            echo $(git add . && git commit -m "remove($2): :coffin: $3 $4 $5 $6 $7 $8 $9" && git push)
             ;;
         h)
             echo "Descripcion del HotFix"
-            echo $(git add . && git commit -m "hotfix($scope): 🚑 $3 $4 $5 $6 $7 $8 $9" && git push)
+            echo $(git add . && git commit -m "hotfix($2): 🚑 $3 $4 $5 $6 $7 $8 $9" && git push)
             ;;
         s)
             echo "Descripcion del Style"
-            echo $(git add . && git commit -m "style($scope): 💄 $3 $4 $5 $6 $7 $8 $9" && git push)
+            echo $(git add . && git commit -m "style($2): 💄 $3 $4 $5 $6 $7 $8 $9" && git push)
             ;;
         q)
             echo "Saliendo del programa..."
@@ -106,7 +167,7 @@ fast_commit() {
 # Inicio del programa
 # Valida sí tiene mas de 2 argumentos
 if [ $# -gt 2 ]; then
-    fast_commit $1 $2 $3 $4 $5 $6 $7 $8 $9
+    fast_commit 
 else
   procesar_opcion "$opcion"
   echo
