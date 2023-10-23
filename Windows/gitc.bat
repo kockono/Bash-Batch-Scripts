dece49b68c795f2bf9f28503e679a66f47b49011
kockono <kockonoop@gmail.com>
2023-10-22 23:38:10 -0600

    🔨 chore(gitc.bat): add support for 'p' type of commit to handle dependencies updates 🚀 feat(gitc.bat): add support for 'p' type of commit to handle dependencies updates

--- Windows/gitc.bat
+++ Windows/gitc.bat
@@ -28,6 +28,9 @@ if "%tipo_commit%"=="f" (
 ) else if "%tipo_commit%"=="w" (
     echo Working:
     git commit -m ":construction: working(%2): %3 %4 %5 %6 %7 %8 %9" && git push
+) else if "%tipo_commit%"=="p" (
+    echo Dependencies:
+    git commit -m ":package: chore(%2): %3 %4 %5 %6 %7 %8 %9" && git push
 ) else if "%tipo_commit%"=="q" (
     echo Saliendo del programa...
     exit 0
